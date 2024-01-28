d6ad40b3bf7bb5037e64e4374b9a7439d9f2eff5
ambersjliu <90348951+ambersjliu@users.noreply.github.com>
2024-01-28 03:20:00 -0500

    site is in workable state

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script>
 	import title from "$lib/images/wordart.png"
-	import imac from "$lib/images/imac.jpg"
+	import imac from "$lib/images/imac.gif"
 	import burger from "$lib/images/burgermouse.jpg"
 	import burgercat from "$lib/images/burgercat.jpg"
 	import nokia from "$lib/images/nokia.jpg"
@@ -9,9 +9,25 @@
 	import home from "$lib/images/home.gif"
 	import winter from "$lib/images/winter.gif"
 	import catslove from "$lib/images/catslove.gif"
+	import folder from "$lib/images/folder.gif"
+	import clown from "$lib/images/clown.gif"
+	import dj from "$lib/images/dj.gif"
+	import minidisc from "$lib/images/minidisc.jpg"
+	import discman from "$lib/images/discman.jpg"
+	import bonzi_glasses from "$lib/images/bonzi-glasses.gif"
+	import tamagotchi from "$lib/images/tamagotchi.gif"
+	import pokemon from "$lib/images/pokemon.gif"
+	import ffvii from "$lib/images/ffvii.gif"
+	import irc from "$lib/images/irc.gif"
+	import dialup from "$lib/images/dialup.gif"
 	import ie from "$lib/images/ie.jpg"
+	import netscape from "$lib/images/netscape.jpg"
+
 	import "../app.css"
 </script>
+
+<img id="dj" src={dj} alt="A DJ">
+<img id="clown" src={clown} alt="A clown">
 
 <div id="page-content">
 	<div id="title-div">
@@ -54,7 +70,7 @@
 			</div>
 			<div class="item-desc">
 				<h3>Burger Mouse</h3>
-				<p>A mouse shaped like a burger...</p>
+				<p>A mouse shaped like a burger...I think technology peaked here...</p>
 			</div>
 		</div>
 		<div class="item">
@@ -86,9 +102,92 @@
 				<p>This was the first AI. The grandfather of ChatGPT. Almighty and all-knowing.</p>
 			</div>
 		</div>
-	<h2>Audio Players</h2>
+		<div class="item">
+			<div class="item-image">
+				<img class="item-image-img" src={bonzi_glasses} alt="The desktop assistant BonziBuddy">
+			</div>
+			<div class="item-desc">
+				<h3>BonziBuddy</h3>
+				<p>He may have been adware and spyware, but also he was just a little guy, doing his best...</p>
+			</div>
+		</div>
+	<h2>Audio Devices</h2>
+		<div class="item">
+			<div class="item-image">
+				<img class="item-image-img" src={minidisc} alt="A Sony Minidisc Player">
+			</div>
+			<div class="item-desc">
+				<h3>Sony MiniDisc Player</h3>
+				<p>This thing is like a little animal...</p>
+			</div>
+		</div>
+		<div class="item">
+			<div class="item-image">
+				<img class="item-image-img" src={discman} alt="A Sony Discman">
+			</div>
+			<div class="item-desc">
+				<h3>Sony Discman</h3>
+				<p>I WANT A CD PLAYER SO BAD!!! I BOUGHT SOME CDs FOR CHEAP AND I DON'T HAVE ANYWHERE TO PLAY THEM!</p>
+			</div>
+		</div>
 	<h2>Toys</h2>
+		<div class="item">
+			<div class="item-image">
+				<img class="item-image-img" src={tamagotchi} alt="Photos of some tamagotchi virtual pets">
+			</div>
+			<div class="item-desc">
+				<h3>Tamagotchi</h3>
+				<p>The urge to have a little creature with you everywhere...</p>
+			</div>
+		</div>
 	<h2>Video Games</h2>
+		<div class="item">
+			<div class="item-image">
+				<img class="item-image-img" src={pokemon} alt="Gif of pokemon blue gameplay">
+			</div>
+			<div class="item-desc">
+				<h3>Pokémon</h3>
+				<p>I'm just saying, 7 year old me could name all 151 pokemon from memory. Easily.</p>
+			</div>
+		</div>
+		<div class="item">
+			<div class="item-image">
+				<img class="item-image-img" src={ffvii} alt="Gif of cloud from ffvii snowboarding">
+			</div>
+			<div class="item-desc">
+				<h3>Final Fantasy VII</h3>
+				<p>I became obsessed with this game despite never playing it even once. It's nostalgic because I liked it in 2019.</p>
+			</div>
+		</div>
 	<h2>Misc.</h2>
+		<div class="item">
+			<div class="item-image">
+				<img class="item-image-img" src={irc} alt="Gif of someone entering an IRC channel">
+			</div>
+			<div class="item-desc">
+				<h3>IRC</h3>
+				<p>I think there's some really nice value in having decentralized, simple chatting 
+					via the Internet. Also, text colours! Discord needs to catch on.
+				</p>
+			</div>
+		</div>
+		<div class="item">
+			<div class="item-image">
+				<img class="item-image-img" src={dialup} alt="Gif of AOL dialup internet">
+			</div>
+			<div class="item-desc">
+				<h3>Dial-up</h3>
+				<p>Maybe I would be sick of not being able to call while using the computer, and 
+					waiting a million years for things to load...but being able to actually hear the handshake
+					"song" between modems is really cool.
+				</p>
+			</div>
+		</div>
+		<footer>
+			<img src={folder} alt="gif of files coming out of a folder" class="small-button">
+			<img src={ie} alt="button that says internet explorer" class="small-button">
+			<img src={netscape} alt="button that says netscape" class="small-button">
+			<img src={folder} alt="gif of files coming out of a folder" class="small-button">
+		</footer>
 </div>
 
